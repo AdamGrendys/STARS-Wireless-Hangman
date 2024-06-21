9c425195d76f00d834f6537bd16accdaef8e4122
Adam Grendys <agrendy@ee69lnx08.ecn.purdue.edu>
2024-06-21 10:24:36 -0400

    afa

--- UART_Rx.sv
+++ UART_Rx.sv
@@ -12,6 +12,7 @@ module uart_rx (
 );
 
 logic [7:0] temp_byte;
+logic [2:0] bit_index;
 curr_state state, next_state;
 
 always_ff @(posedge clk, negedge nRst) begin
@@ -27,18 +28,66 @@ end
 always_comb begin
     case (state)
         IDLE: begin
+            rx_ready = 0;
+            bit_index = 0;
+            temp_byte = 0;
+            bit_index = 0;
+
+            if (rx_serial == 0 && rec_ready)
+                next_state = START;
+            else
+                next_state = IDLE;
         end
         START: begin
+            // wait half of baud cycle here
+            rx_ready = 0;
+            temp_byte = 0;
+            bit_index = 0;
+
+            if (rx_ready == 0 && rec_ready)
+                next_state = DATAIN;
+            else    
+                next_state = START;
         end
         DATAIN: begin
+            // wait for baud cycle 
+            temp_byte[bit_index] = rx_serial;
+            rx_ready = 0;
+
+            if (bit_index < 7) begin
+                bit_index = bit_index + 1;
+                next_state = DATAIN;
+            end else begin
+                bit_index = 0;
+                next_state = PARITY;
+            end
+        end
+        PARITY: begin 
+            /// SANDEEEPPPPP HELPPPPPPPPPP
+            rx_ready = 0;
+            temp_byte = 0;
+            bit_index = 0;
+            next_state = STOP;
         end
         STOP: begin
+            // wait for baud cycle
+            rx_ready = 1;
+            temp_byte = 0;
+            bit_index = 0;
+            next_state = CLEAN;
         end
         CLEAN: begin 
+            rx_ready = 0;
+            temp_byte = 0;
+            bit_index = 0;
+            next_state = IDLE;
         end
         default: begin
+            rx_ready = 0;
+            temp_byte = 0;
+            bit_index = 0;
+            next_state = IDLE;
         end
     endcase
 end
-
 endmodule
