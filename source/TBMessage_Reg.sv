74db304c463e60509a52416dc35bd0168d2b8221
Adam Grendys <agrendy@ee69lnx08.ecn.purdue.edu>
2024-06-24 09:51:45 -0400

    tb work

--- source/TBMessage_Reg.sv
+++ source/TBMessage_Reg.sv
@@ -12,10 +12,10 @@ module tb_msg_reg ();
 
 // Testbench ports
 localparam CLK_PERIOD = 10; // 100 Hz clk
-logic tb_clk, tb_nRst, tb_ready, tb_transmit_ready,
-logic [7:0] tb_data,
-logic tb_blue, tb_tx_ctrl,
-logic [7:0] tb_tx_byte
+logic tb_clk, tb_nRst, tb_ready, tb_transmit_ready;
+logic [7:0] tb_data;
+logic tb_blue, tb_tx_ctrl;
+logic [7:0] tb_tx_byte;
 logic [7:0] tb_msg, tb_msg_rdy;
 curr_state state, next_state;
 
@@ -43,6 +43,54 @@ initial begin
 
     // Wait some time before starting first test case
     #(0.1);
+
+
+    // ***********************************
+    // Test Case 0: Power-on-Reset 
+    // ***********************************
+    #(CLK_PERIOD * 2);
+    @(negedge tb_clk);
+    tb_nRst = 1'b0; 
+    @(negedge tb_clk);
+    @(negedge tb_clk);
+    tb_nRst = 1'b1;
+    @(posedge tb_clk);
+    #(CLK_PERIOD * 2);
+
+
+    // ***********************************
+    // Test Case 1: Take Input, Send
+    // ***********************************
+    tb_ready = 1;
+    #(CLK_PERIOD);
+    tb_transmit_ready = 1;
+    #(CLK_PERIOD * 2);
+
+
+    // ***********************************
+    // Test Case 2: Do not take input
+    // ***********************************
+    tb_nRst = 0;
+    #(CLK_PERIOD);
+    tb_nRst = 1;
+    #(CLK_PERIOD);
+
+    tb_ready = 0;
+    tb_transmit_ready = 0;
+    #(CLK_PERIOD * 3);
+
+
+
+    // ***********************************
+    // Test Case 3: Take Input, Dont Send 
+    // ***********************************
+    tb_ready = 1;
+    tb_transmit_ready = 0;
+    #(CLK_PERIOD * 2)
+    
+    tb_transmit_ready = 1;
+    #(CLK_PERIOD);
+
 
 end
 
