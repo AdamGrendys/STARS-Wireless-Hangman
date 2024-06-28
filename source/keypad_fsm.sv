599d6335eaabfab358d9dadd7b0a2cdb1e9c2390
yejinoh03 <112606871+yejinoh03@users.noreply.github.com>
2024-06-28 11:29:14 -0400

    Revise keypad FSM and begin FPGA testing

--- source/keypad_fsm.sv
+++ source/keypad_fsm.sv
@@ -1,14 +1,22 @@
 module keypad_fsm (
   input logic clk, nRst, strobe,
   input logic [7:0] cur_key, // Concatenation of row and column
+  
+  // Temporarily set for FPGA testing
+  output logic [2:0] state,
+  output logic [7:0] prev_key,
+  output logic unlocked,
+  output logic [7:0] cur_key_out,
+  
   output logic ready, // Notification of letter submission after selection
   output logic game_end, // End-of-game signal
   output logic [7:0] data // ASCII character from current key and number of consecutive presses
 );
-  logic [2:0] state, next_state;
-  logic [7:0] prev_key;
-  assign prev_key = 8'd0;
-  logic [7:0] next_data;
+  // logic [2:0] state;
+  logic [2:0] next_state;
+  logic [7:0] temp_data, next_data;
+  // logic [7:0] prev_key;
+  // logic unlocked;
 
   typedef enum logic [2:0] {
       INIT = 0, S0 = 1, S1 = 2, S2 = 3, S3 = 4, DONE = 5
@@ -17,38 +25,66 @@ module keypad_fsm (
   always_ff @(posedge clk, negedge nRst) begin
     if (~nRst) begin
       state <= INIT;
-      prev_key <= 8'd0;
       ready <= 1'b0;
       data <= 8'd0;
+      
+      prev_key <= 8'd0;
     end else begin
       state <= next_state;
-      prev_key <= cur_key;
       ready <= (next_state == DONE);
       data <= next_data;
+      
+      if (unlocked) // Prevent loading too early
+        prev_key <= cur_key;
     end
   end
-
+  
+  // 4-letter sets
   localparam key_7 = 8'b00101000; // R2 C0
   localparam key_9 = 8'b00100010; // R2 C2
 
+  // Valid non-letter sets
   localparam submit_letter_key = 8'b00011000; // R3 C0
   localparam clear_key = 8'b00010100; // R3 C1
   localparam submit_word_key = 8'b00010010; // R3 C2
   localparam game_end_key = 8'b00100001; // R2 C3
+  
+  // Invalid non-letter sets
+  localparam key_1 = 8'b10001000; // R0 C0
+  localparam key_A = 8'b10000001; // R0 C3
+  localparam key_B = 8'b01000001; // R1 C3
+  localparam key_D = 8'b00010001; // R3 C3
+  
+  // Handle ASCII character conversion
+  ascii_encoder encoder (.row (cur_key[7:4]),
+                         .col (cur_key[3:0]),
+                         .state (state),
+                         .ascii_character (temp_data));
 
-  // TODO: game_end signal should make state return to INIT
-
+  // TODO: Check if ready signal high at right moment
+  
   always_comb begin
-    // 0. By default
-    next_state = state;
+    // 0-1. By default
+    //next_state = state;
     next_data = data;
+    game_end = 1'b0;
+    unlocked = 1'b0;
 
-    // 1. Invalid (inactive) or no push button pressed
-    if ((!strobe) ||
-      (cur_key == (8'b10000001 | 8'b01000001 | 8'b00010001 | 8'b10001000)) ||
-      (cur_key == submit_word_key)) begin
+    // 0-2. No push button pressed
+    if (!strobe) begin
       next_state = state;
-      // TODO: Test Case: Make sure state doesn't change with invalid key as input
+    
+    // 1. Invalid (inactive) push button pressed
+    end else if ((cur_key == key_1) ||
+       (cur_key == key_A) ||
+       (cur_key == key_B) ||
+       (cur_key == key_D) ||
+       (cur_key == submit_word_key)) begin
+      // Test Case: Make sure state doesn't change with invalid key as input
+      // Invalid: 00 (1), 03 (A), 13 (B), 33 (D)
+      // Submit Word: 23 (C)
+      next_state = state;
+      unlocked = 1'b0;
 
     // 2. Valid (active) push button pressed
     end else begin
@@ -57,11 +93,15 @@ module keypad_fsm (
       end
 
       // Listing valid push button scenarios
-      // 2-1. CLEAR
+      // 2-1. CLEAR or GAME END
       // Should take priority over other push buttons
-      if (cur_key == clear_key) begin
+      if ((cur_key == clear_key) || 
+          (cur_key == game_end_key)) begin
         next_state = INIT;
-        next_data = 8'b0;
+        next_data = 8'd0;
+
+        if (cur_key == game_end_key)
+          game_end = 1'b1;
 
       // 2-2. SUBMIT_LETTER
       end else if ((cur_key == submit_letter_key) && (state != INIT)) begin
@@ -78,6 +118,8 @@ module keypad_fsm (
             next_state = S2;
           end else if (state == S2) begin
             next_state = ((cur_key == key_7) || (cur_key == key_9)) ? (S3) : (S0);
+          end else if (state == S3) begin
+            next_state = S0;
           end
 
         // 2-3-2. Buttons do not match (first or new letter set selected)
@@ -88,36 +130,13 @@ module keypad_fsm (
         end
 
         // Update pre-submission data (current letter) to preview on display each time
-        next_data = get_ascii_from_key(cur_key[7:4], cur_key[3:0]) + ({5'd0, state} - 1);
+        next_data = temp_data;
       end
+      
+      // Allow cur_key to be loaded into prev_key (on the next strobe) only after cur_key has been initialized, post-reset
+      unlocked = 1'b1;
     end
-  end
-
-  function logic [7:0] get_ascii_from_key (logic [3:0] row, logic [3:0] col);
-    logic [7:0] val;
-
-    if (row[0]) begin
-      if (col[1])
-        val = 8'd65;
-      else
-        val = 8'd68;
-
-    end else if (row[1]) begin
-      if (col[0])
-        val = 8'd71;
-      else if (col[1])
-        val = 8'd74;
-      else
-        val = 8'd77;
     
-    end else if (row[2]) begin
-      if (col[0])
-        val = 8'd80;
-      else if (col[1])
-        val = 8'd84;
-      else
-        val = 8'd87;
-    end
-    // return val;
-  endfunction
+    cur_key_out = cur_key;
+  end
 endmodule
--- top.sv
+++ top.sv
@@ -5,8 +5,7 @@ module top (
   // I/O ports
   input  logic hz100, reset,
   input  logic [20:0] pb,
-  output logic [7:0] left, right,
-         ss7, ss6, ss5, ss4, ss3, ss2, ss1, ss0,
+  output logic [7:0] left, right, ss7, ss6, ss5, ss4, ss3, ss2, ss1, ss0,
   output logic red, green, blue,
 
   // UART ports
@@ -17,69 +16,69 @@ module top (
 );
 
   // Your code goes here...
-  logic [7:0] input_key, ascii_char;
-  logic [3:0] discard_scan_col, discard_row, discard_col;
+  logic [7:0] input_key, cur_out;
+  logic discard_strobe;
+  logic [3:0] discard_scan_col, discard_row, discard_col, sel_row_out, sel_col_out;
 
   keypad_controller kc (.clk (hz100),
                         .nRst (~pb[19]), // Single key for simplicity
                         .read_row (pb[7:4]),
                         .cur_key (input_key), // Input for FSM
-                        .strobe (green), // Input for FSM
+                        .strobe (discard_strobe), // Input for FSM
                         .scan_col (discard_scan_col),
-                        .sel_row (left[7:4]), //left[7:4]), // Temporary
-                        .sel_col (left[3:0])); //left[3:0])); // Temporary
+                        .read_col_in (pb[3:0]),
+
+                        .sel_row (sel_row_out), //left[7:4]), // Temporary
+                        .sel_col (sel_col_out)); //left[3:0])); // Temporary
+
+  logic [7:0] row_col;
+  assign row_col = {sel_row_out, sel_col_out};
+  assign discard_strobe = green;
 
   // Row (sel_row)
-  ssdec ssdec7 (.in (left[7:4]),
+  ssdec ssdec7 (.in (row_col[7:4]), //(row_col[7:4]),
                 .enable (1'b1),
                 .out (ss7[6:0]));
 
   // Column (sel_col)
-  ssdec ssdec6 (.in (left[3:0]),
+  ssdec ssdec6 (.in (row_col[3:0]), //(row_col[3:0]),
                 .enable (1'b1),
                 .out (ss6[6:0]));
+                
+  ssdec ssdec4 (.in (prev_key_out[7:4]),
+                .enable (1'b1),
+                .out (ss4[6:0]));
+                
+  ssdec ssdec3 (.in (prev_key_out[3:0]),
+                .enable (1'b1),
+                .out (ss3[6:0]));
 
   logic discard_game_end;
-  logic [7:0] discard_data;
-  logic [7:0] char;
-  
-  ascii_encoder ae (.row (input_key[7:4]), .col (input_key[3:0]), .state (3'd0), .ascii_character (char));
+  logic [7:0] discard_data, data_out, discard_temp_data, prev_key_out;
+  logic [2:0] state_out;
 
-  assign right[7:0] = char;
-
-/*
   keypad_fsm key_fsm (.clk (hz100),
                       .nRst (~pb[19]), // Single key for simplicity
-                      .strobe (green), // Input from controller
-                      .key (input_key), // Input from controller
+                      .strobe (discard_strobe), // Input from controller
+
+                      .cur_key (input_key), // Input from controller
+                      //.cur_key_out (cur_out),
+                      .prev_key (prev_key_out),
                       .ready (red), // Output
-                      .data (discard_data), // Output
-                      .temp_data (right[7:0]),
-                      .cur_key (left[7:0]),
-                      .game_end (discard_game_end)); // TODO: Output
-*/
-  //assign right [7:0] = input_key;
+                      .data (data_out),//right[7:0]), // Output
+                      .state (state_out),
+                      .unlocked (left[7]),
+                      .game_end (blue)); // TODO: Output
 
-  //ssdec_original
-  /*
-  ssdec ssdec1 (.in (right[7:4]),
-                         .enable (1'b1),
-                         .out (ss1[6:0]));
+  ssdec_original ssdec0 (.in ({1'b0, state_out}),
+                .enable (1'b1),
+                .out (ss0[6:0]));
+                
+  //assign = right[7:0];
+  //assign left[7:0] = data_out;
+  assign right[7:0] = data_out;
+  //assign right[2:0] = state_out;
 
-  //ssdec_original
-  ssdec ssdec0 (.in (right[3:0]),
-                         .enable (1'b1),
-                         .out (ss0[6:0]));
-*/
-  /*
-  ssdec_original ssdec3 (.in (temp[3:0]),
-                          .enable (1'b1),
-                          .out (ss3[6:0]));
-
-  ssdec_original ssdec4 (.in (temp[7:4]),
-                          .enable (1'b1),
-                          .out (ss4[6:0]));
-  */
 endmodule
 
 // Add more modules down here...
@@ -126,6 +125,7 @@ module ssdec (
         4'b0100: out = 7'b0000110; // 1st row/column
         4'b0010: out = 7'b1011011; // 2nd row/column
         4'b0001: out = 7'b1001111; // 3rd row/column
+        4'd0: out = 7'b0001000;
         default: out = 7'd0;
       endcase
     else // Turned off - push button unpressed
@@ -135,13 +135,14 @@ endmodule
 
 module keypad_controller (
   input logic clk, nRst,
-  input logic [3:0] read_row,
+  input logic [3:0] read_row, read_col_in,
   output logic [7:0] cur_key, // Input for keypad_fsm
   output logic strobe, // Input for keypad_fsm
-  output logic [3:0] scan_col, sel_col, sel_row
+  output logic [3:0] scan_col, 
+  output logic [3:0] sel_col, sel_row
 );
   logic [3:0] Q0, Q1, Q1_delay;
-  logic [3:0] scan_col_next, sel_col_next;
+  logic [3:0] scan_col_next, sel_col_next, read_col;
 
   // Synchronizer and rising (positive) edge detector - 3 FFs
   always_ff @(posedge clk, negedge nRst) begin
@@ -150,6 +151,8 @@ module keypad_controller (
       Q0 <= 4'd0;
       Q1 <= 4'd0;
       Q1_delay <= 4'd0;
+      
+      read_col <= 4'd0;
 
       // Note: Deactivating the scanning of columns should prevent key input
       scan_col <= 4'd0;
@@ -163,13 +166,15 @@ module keypad_controller (
       Q0 <= read_row;
       Q1 <= Q0;
       Q1_delay <= Q1;
+      
+      read_col <= read_col_in;
 
       // Variables for testing purposes
       // Strobe should prompt transition in finite state machine (FSM) module
       // Only if there is an active column, on positive edge of button press (row)
-      if ((strobe) & (|scan_col)) begin
+      if ((strobe) & (|read_col)) begin// (|scan_col)) begin
         sel_row <= read_row;
-        sel_col <= scan_col_next;
+        sel_col <= read_col_in; //scan_col_next;
       end
 
       // Active column changes every clock cycle
@@ -201,20 +206,29 @@ module keypad_controller (
   end
 
   assign strobe = |((~Q1_delay) & (Q1));
-  assign cur_key = ((|read_row) & (|scan_col)) ? ({read_row, scan_col}) : (8'd0);
+  assign cur_key = ((|read_row) & (|read_col_in)) ? ({read_row, read_col_in}) : (8'd0);
+  //((|read_row) & (|scan_col)) ? ({read_row, scan_col}) : (8'd0);
 endmodule
 
 module keypad_fsm (
   input logic clk, nRst, strobe,
-  input logic [7:0] key, // Concatenation of row and column
+  input logic [7:0] cur_key, // Concatenation of row and column
+  
+  // Temporarily set for FPGA testing
+  output logic [2:0] state,
+  output logic [7:0] prev_key,
+  output logic unlocked,
+  output logic [7:0] cur_key_out,
+  
   output logic ready, // Notification of letter submission after selection
   output logic game_end, // End-of-game signal
-  output logic [7:0] data, cur_key, temp_data // ASCII character from current key and number of consecutive presses
+  output logic [7:0] data // ASCII character from current key and number of consecutive presses
 );
-  logic [2:0] state, next_state;
-  logic [7:0] prev_key; //, delayed_key;
-  assign prev_key = 8'd0;
-  logic [7:0] next_data;
+  // logic [2:0] state;
+  logic [2:0] next_state;
+  logic [7:0] temp_data, next_data;
+  // logic [7:0] prev_key;
+  // logic unlocked;
 
   typedef enum logic [2:0] {
       INIT = 0, S0 = 1, S1 = 2, S2 = 3, S3 = 4, DONE = 5
@@ -223,61 +237,66 @@ module keypad_fsm (
   always_ff @(posedge clk, negedge nRst) begin
     if (~nRst) begin
       state <= INIT;
-
-      //delayed_key <= 8'd0;
-      //cur_key <= 8'd0;
-      //prev_key <= 8'd0;
-
       ready <= 1'b0;
       data <= 8'd0;
-    end else begin
-      state <= next_state;
-
-      //delayed_key <= key;
-      //cur_key <= delayed_key;
-      //prev_key <= cur_key;
-
-      ready <= (next_state == DONE);
-      data <= next_data;
-    end
-  end
-
-  always_ff @(posedge strobe, negedge nRst) begin
-    if (~nRst) begin
-      //delayed_key <= 8'd0;
-      cur_key <= 8'd0;
+      
       prev_key <= 8'd0;
     end else begin
-      //delayed_key <= key;
-      cur_key <= key;
-      prev_key <= cur_key;
+      state <= next_state;
+      ready <= (next_state == DONE);
+      data <= next_data;
+      
+      if (unlocked) // Prevent loading too early
+        prev_key <= cur_key;
     end
   end
-
+  
+  // 4-letter sets
   localparam key_7 = 8'b00101000; // R2 C0
   localparam key_9 = 8'b00100010; // R2 C2
 
+  // Valid non-letter sets
   localparam submit_letter_key = 8'b00011000; // R3 C0
   localparam clear_key = 8'b00010100; // R3 C1
   localparam submit_word_key = 8'b00010010; // R3 C2
   localparam game_end_key = 8'b00100001; // R2 C3
   
+  // Invalid non-letter sets
+  localparam key_1 = 8'b10001000; // R0 C0
+  localparam key_A = 8'b10000001; // R0 C3
+  localparam key_B = 8'b01000001; // R1 C3
+  localparam key_D = 8'b00010001; // R3 C3
+  
+  // Handle ASCII character conversion
   ascii_encoder encoder (.row (cur_key[7:4]),
                          .col (cur_key[3:0]),
                          .state (state),
                          .ascii_character (temp_data));
 
+  // TODO: Check if ready signal high at right moment
+  
   always_comb begin
-    // 0. By default
-    next_state = state;
+    // 0-1. By default
+    //next_state = state;
     next_data = data;
+    game_end = 1'b0;
+    unlocked = 1'b0;
 
-    // 1. Invalid (inactive) or no push button pressed
-    if ((!strobe) ||
-      (cur_key == (8'b10000001 | 8'b01000001 | 8'b00010001 | 8'b10001000)) ||
-      (cur_key == submit_word_key)) begin
+    // 0-2. No push button pressed
+    if (!strobe) begin
       next_state = state;
+    
+    // 1. Invalid (inactive) push button pressed
+    end else if ((cur_key == key_1) ||
+       (cur_key == key_A) ||
+       (cur_key == key_B) ||
+       (cur_key == key_D) ||
+       (cur_key == submit_word_key)) begin
       // Test Case: Make sure state doesn't change with invalid key as input
+      // Invalid: 00 (1), 03 (A), 13 (B), 33 (D)
+      // Submit Word: 23 (C)
+      next_state = state;
+      unlocked = 1'b0;
 
     // 2. Valid (active) push button pressed
     end else begin
@@ -286,11 +305,15 @@ module keypad_fsm (
       end
 
       // Listing valid push button scenarios
-      // 2-1. CLEAR
+      // 2-1. CLEAR or GAME END
       // Should take priority over other push buttons
-      if (cur_key == clear_key) begin
+      if ((cur_key == clear_key) || 
+          (cur_key == game_end_key)) begin
         next_state = INIT;
-        next_data = 8'b0;
+        next_data = 8'd0;
+
+        if (cur_key == game_end_key)
+          game_end = 1'b1;
 
       // 2-2. SUBMIT_LETTER
       end else if ((cur_key == submit_letter_key) && (state != INIT)) begin
@@ -307,6 +330,8 @@ module keypad_fsm (
             next_state = S2;
           end else if (state == S2) begin
             next_state = ((cur_key == key_7) || (cur_key == key_9)) ? (S3) : (S0);
+          end else if (state == S3) begin
+            next_state = S0;
           end
 
         // 2-3-2. Buttons do not match (first or new letter set selected)
@@ -319,9 +344,13 @@ module keypad_fsm (
         // Update pre-submission data (current letter) to preview on display each time
         next_data = temp_data;
       end
+      
+      // Allow cur_key to be loaded into prev_key (on the next strobe) only after cur_key has been initialized, post-reset
+      unlocked = 1'b1;
     end
+    
+    cur_key_out = cur_key;
   end
-
 endmodule
 
 module ascii_encoder (
@@ -355,8 +384,8 @@ module ascii_encoder (
       else if (col[1]) // "2" - 0010
         ascii_character = 8'd87;
     end
-
-    //if (|{row, col})
-    //  ascii_character += ({5'd0, state} - 8'd1);
+    
+    if ((1 <= state) && (state <= 4)) // S0 through S3
+      ascii_character += ({5'd0, state} - 8'd1);
   end
 endmodule
