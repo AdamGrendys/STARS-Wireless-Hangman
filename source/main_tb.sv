34210e0f44ebb2ea5946807062b2fcb8298b5fb8
Sandeep Saravanakumar <sarava16@ee69lnx07.ecn.purdue.edu>
2024-07-02 19:54:02 -0400

    done check

--- source/main_tb.sv
+++ source/main_tb.sv
@@ -132,6 +132,36 @@ initial begin
 
     #(CLK_PERIOD * 100000);
 
+    @(negedge tb_clk);
+    tb_row_host = 4'd0;
+
+    //end pressing L, Start pressing E 
+
+    #(CLK_PERIOD * 300000);
+    tb_row_host = 4'b1000; //for E
+    #(CLK_PERIOD * 100000);
+
+    @(negedge tb_clk);
+    tb_row_host = 4'd0;
+
+    #(CLK_PERIOD * 400000);
+    tb_row_host = 4'b1000; // 'E'
+    #(CLK_PERIOD * 100000);
+
+    @(negedge tb_clk);
+    tb_row_host = 4'd0;
+
+    @(posedge tb_clk);
+    #(CLK_PERIOD * 300000); // R3 C0 (submit_letter_key)
+    tb_row_host = 4'b0001;
+
+    #(CLK_PERIOD * 100000);
+
+    @(negedge tb_clk);
+    tb_row_host = 4'd0;
+
+
+
 
 
 
