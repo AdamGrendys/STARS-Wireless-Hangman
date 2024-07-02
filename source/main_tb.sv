d9a2f7f1961be1b66829149ac2c60080f1bf8896
Adam Grendys <agrendy@ee69lnx08.ecn.purdue.edu>
2024-07-02 15:31:33 -0400

    k

--- source/main_tb.sv
+++ source/main_tb.sv
@@ -79,7 +79,7 @@ initial begin
     tb_row_host = 4'd0;
 
     @(posedge tb_clk);
-    #(CLK_PERIOD * 300000); // R3 C0 (submit_letter_key)
+    #(CLK_PERIOD * 400000); // R3 C0 (submit_letter_key)
     tb_row_host = 4'b0001;
 
     #(CLK_PERIOD * 200000);
