b93a1a6c5f09dab1284e0b01b9a73912efcbeafe
Justin Sanchez <sanch431@ee69lnx07.ecn.purdue.edu>
2024-07-03 13:58:49 -0400

    r

--- source/main_tb.sv
+++ source/main_tb.sv
@@ -363,21 +363,21 @@ initial begin
 
     // PRESS A
 
-    tb_row_player = 4'b1000; // R0 C1 -> 'A'
+    tb_row_host = 4'b1000; // R0 C1 -> 'A'
 
     #(CLK_PERIOD * 100000);
 
     @(negedge tb_clk);
-    tb_row_player = 4'd0;
+    tb_row_host = 4'd0;
 
     @(posedge tb_clk);
     #(CLK_PERIOD * 300000); // R3 C0 (submit_letter_key)
-    tb_row_player = 4'b0001;
+    tb_row_host = 4'b0001;
 
     #(CLK_PERIOD * 100000);
 
     @(negedge tb_clk);
-    tb_row_player = 4'd0;
+    tb_row_host = 4'd0;
      #(CLK_PERIOD * 400000);
 
     // PRESS R 
