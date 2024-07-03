9b199bad9fcca07d54d5d63b755c823dd1238827
Sandeep Saravanakumar <sarava16@ee69lnx07.ecn.purdue.edu>
2024-07-02 20:51:54 -0400

    done check

--- source/main_tb.sv
+++ source/main_tb.sv
@@ -178,21 +178,21 @@ initial begin
 
     // GUESS first letter P
     #(CLK_PERIOD * 400000);
-    tb_row_host = 4'b0010; // R2 C0 -> 'P'
+    tb_row_player = 4'b0010; // R2 C0 -> 'P'
 
     #(CLK_PERIOD * 100000);
 
     @(negedge tb_clk);
-    tb_row_host = 4'd0;
+    tb_row_player = 4'd0;
 
     @(posedge tb_clk);
     #(CLK_PERIOD * 400000); // R3 C0 (submit_letter_key)
-    tb_row_host = 4'b0001;
+    tb_row_player = 4'b0001;
 
     #(CLK_PERIOD * 100000);
 
     @(negedge tb_clk);
-    tb_row_host = 4'd0;
+    tb_row_player = 4'd0;
     #(CLK_PERIOD * 400000);
 
 
