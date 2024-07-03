806e0a0e3e73820b68a393029c9b7794bae4b5fc
Justin Sanchez <sanch431@ee69lnx07.ecn.purdue.edu>
2024-07-03 10:37:48 -0400

    r

--- source/main_tb.sv
+++ source/main_tb.sv
@@ -235,6 +235,71 @@ initial begin
     @(negedge tb_clk);
     tb_row_player = 4'd0;
      #(CLK_PERIOD * 400000);
+
+    // press E
+
+    #(CLK_PERIOD * 600000);
+    tb_row_host = 4'b1000; //for E
+    #(CLK_PERIOD * 100000);
+
+    @(negedge tb_clk);
+    tb_row_host = 4'd0;
+
+    #(CLK_PERIOD * 400000);
+    tb_row_host = 4'b1000; // 'E'
+    #(CLK_PERIOD * 100000);
+
+    @(negedge tb_clk);
+    tb_row_host = 4'd0;
+
+    @(posedge tb_clk);
+    #(CLK_PERIOD * 200000); // R3 C0 (submit_letter_key)
+    tb_row_host = 4'b0001;
+
+    #(CLK_PERIOD * 100000);
+
+    @(negedge tb_clk);
+    tb_row_host = 4'd0;
+    #(CLK_PERIOD * 100000);
+
+    #(CLK_PERIOD * 500000);
+     tb_row_host = 4'b0001;
+     #(CLK_PERIOD * 100000);
+     @(negedge tb_clk);
+    tb_row_host = 4'd0;
+    #(CLK_PERIOD * 400000);
+
+    // presss L 
+
+    #(CLK_PERIOD * 100000);
+    tb_row_host = 4'b0100; // R1 C1 -> 'L'
+    #(CLK_PERIOD * 100000);
+
+    @(negedge tb_clk);
+    tb_row_host = 4'd0;
+
+    #(CLK_PERIOD * 400000);
+    tb_row_host = 4'b0100; // R1 C1 -> 'L'
+    #(CLK_PERIOD * 100000);
+
+    @(negedge tb_clk);
+    tb_row_host = 4'd0;
+
+     #(CLK_PERIOD * 400000);
+    tb_row_host = 4'b0100; // R1 C1 -> 'L'
+    #(CLK_PERIOD * 100000);
+
+    @(negedge tb_clk);
+    tb_row_host = 4'd0;
+
+     @(posedge tb_clk);
+    #(CLK_PERIOD * 300000); // R3 C0 (submit_letter_key)
+    tb_row_host = 4'b0001;
+
+    #(CLK_PERIOD * 100000);
+
+    @(negedge tb_clk);
+    tb_row_host = 4'd0;
     
 
 
